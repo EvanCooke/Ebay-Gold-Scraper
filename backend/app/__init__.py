928429f83ea715760ff9017149b54c58fd39be4e
Evan Cooke <evncooke@gmail.com>
2025-04-30 17:20:23 -0500

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from backend.app.routes import notifications_bp, frontend_bp
-from backend.instance.config import Config
+from instance.config import Config
 
 def create_app():
     app = Flask(__name__)
